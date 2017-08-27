e36ee13073b735455dbad070f10e2c91e8e3dab6
giacomocusinato <giacomo.cusinato@outlook.com>
2017-08-27 23:26:38 +0100

    [ADD] Implement state format logic Format retrieved weather json in order to match application state

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,31 +4,61 @@ import { ROOT_URL } from '../constants/ApiUrl';
 
 export function fetchWeather() {
   return dispatch => {
-
     dispatch(requestWeather());
 
-    return fetch(`${ROOT_URL}&q=London,US`)
+    return fetch(`${ROOT_URL}&q=London,GB`)
       .then(response => response.json())
-      .then(json => dispatch(receiveWeatherSuccess(json)))
-      .catch(() => dispatch(receiveWeatherError()))
-  }
+      .then(json => {
+        /*
+        Format json to the designen application state, eg:
+
+        {
+          city: 'London, UK',
+          current: {
+            // Api weather object for current hour (first item)
+          }
+          days: [
+            date: date
+            weather: [ // Api weather object list for given date ]
+          ]
+        }
+        */
+
+        let initialValue = {
+          city: json.city,
+          current: json.list[0],
+          days: []
+        };
+
+        let weatherObject = json.list.reduce((obj, item) => {
+          let date = new Date(item.dt_txt);
+          if (!obj.days.length) {
+            obj.days.push({ date, list: [item] });
+          } else {
+            obj.days[obj.days.length - 1].date.getDay() === date.getDay()
+              ? obj.days[obj.days.length - 1].list.push(item)
+              : obj.days.push({ date, list: [item] });
+          }
+          return obj;
+        }, initialValue);
+
+        dispatch(receiveWeatherSuccess(weatherObject));
+      })
+      .catch((err) => {
+        console.log(err);
+        dispatch(receiveWeatherError());
+      });
+  };
 }
 
 export function requestWeather() {
-  return {
-    type: types.REQUEST_WEATHER
-  }
+  return { type: types.REQUEST_WEATHER };
 }
 
 export function receiveWeatherSuccess(weather) {
-  return {
-    type: types.RECEIVE_WEATHER_SUCCESS,
-    weather
-  }
+  return { type: types.RECEIVE_WEATHER_SUCCESS, weather };
 }
 
 export function receiveWeatherError() {
-  return {
-    type: types.RECEIVE_WEATHER_ERROR
-  }
+  return { type: types.RECEIVE_WEATHER_ERROR };
 }
