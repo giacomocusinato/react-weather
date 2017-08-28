cd48aa64f9179815092d281d4e4d8fb14be59ccb
giacomocusinato <giacomo.cusinato@outlook.com>
2017-08-28 15:39:05 +0100

    [ADD] Skycon component

---
+++ src/components/Skycon.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+const Skycons = require('skycons')(window);
+
+
+class Skycon extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      skycons: new Skycons({'color': '#333'})
+    };
+  }
+
+  componentDidMount() {
+    this.state.skycons.add(
+      ReactDOM.findDOMNode(this), Skycons[this.props.icon]
+    );
+
+    this.state.skycons.play();
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.state.skycons.set(ReactDOM.findDOMNode(this), Skycons[nextProps.icon]);
+  }
+
+  componentWillUnmount() {
+    this.state.skycons.pause();
+    this.state.skycons.remove(ReactDOM.findDOMNode(this));
+  }
+
+  render() {
+    return (
+      <canvas className="skycon" />
+    );
+  }
+}
+
+
+export default Skycon;
