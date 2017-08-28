5c038a12e389d42155f99bb62fe6b307e903f99f
giacomocusinato <giacomo.cusinato@outlook.com>
2017-08-28 15:39:16 +0100

    [ADD] Current weather work

--- src/components/Current.jsx
+++ src/components/Current.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import Skycon from './Skycon';
+
 
 class Current extends Component {
   render() {
@@ -15,7 +17,18 @@ class Current extends Component {
     const { location, current } = this.props.weather.data;
 
     return (
-      <p>{location.name}</p>
+      <div className="current-container">
+        <div className="row justify-content-center">
+          <Skycon icon="PARTLY_CLOUDY_DAY" />
+          <p className="weather">
+            {Math.round(current.main.temp)}° {current.weather[0].main}
+          </p>
+        </div>
+        <p className="description">
+          <span>{current.weather[0].description} </span>
+           in {location.name}, {location.country}
+        </p>
+      </div>
     );
   }
 
