7fb84761aae55fb27f921e37132dc4c58a8d0b59
giacomocusinato <giacomo.cusinato@outlook.com>
2017-08-28 11:19:43 +0100

    [REF] Refactor object assignement in weather reducer

--- src/reducers/weather.js
+++ src/reducers/weather.js
@@ -1,13 +1,21 @@
 import * as types from '../constants/ActionTypes';
 
-export default (state = null, action) => {
+export default (state = {}, action) => {
   switch (action.type) {
     case types.REQUEST_WEATHER:
-      return state;
+      return Object.assign({}, {
+         isLoading: true,
+       });
     case types.RECEIVE_WEATHER_SUCCESS:
-      return state;
+      return Object.assign({}, state, {
+         data: action.weather,
+         isLoading: false,
+         hasErrored: false
+       });
     case types.RECEIVE_WEATHER_ERROR:
-      return state;
+      return Object.assign({}, {
+         hasErrored: true,
+       });
     default:
       return state;
   }
