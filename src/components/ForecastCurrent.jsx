65c61aa8696a9e120dab4f12be46ec0ccc5b53b8
giacomocusinato <giacomo.cusinato@outlook.com>
2017-08-29 09:10:03 +0100

    [ADD] Map weather condition to Skycons

--- src/components/ForecastCurrent.jsx
+++ src/components/ForecastCurrent.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Skycon from './Skycon';
+import { getSkycon } from '../utils/WeatherIconUtils';
 
 
 class ForecastCurrent extends Component {
@@ -10,7 +11,7 @@ class ForecastCurrent extends Component {
     return (
       <div className="current-container">
         <div className="row justify-content-center">
-          <Skycon icon="PARTLY_CLOUDY_DAY" />
+          <Skycon icon={getSkycon(current.weather[0].icon)} />
           <p className="weather">
             {Math.round(current.main.temp)}° {current.weather[0].main}
           </p>
---
+++ src/utils/WeatherIconUtils.js
@@ -0,0 +1,33 @@
+/*
+  Maps open weather icons to Skycons
+*/
+export function getSkycon(icon) {
+  console.log(icon);
+  switch(icon) {
+    case '01d':
+      return 'CLEAR_DAY';
+    case '01n':
+      return 'CLEAR_NIGHT';
+    case '02d':
+      return 'PARTLY_CLOUDY_DAY';
+    case '02n':
+      return 'PARTLY_CLOUDY_NIGHT';
+    case '03d':
+    case '03n':
+    case '04d':
+    case '04n':
+      return 'CLOUDY';
+    case '09d':
+    case '09n':
+    case '10d':
+    case '10n':
+    case '11d':
+    case '11n':
+      return 'RAIN';
+    case '50d':
+    case '50n':
+      return 'FOG';
+    default:
+      return 'CLOUDY';
+  }
+}
