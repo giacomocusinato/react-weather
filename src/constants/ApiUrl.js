253a809c4d2d7f4fc0c45b3cba084a57c53e34a3
giacomocusinato <giacomo.cusinato@outlook.com>
2017-08-30 09:38:55 +0100

    [ADD] Use HTTPS on api url

--- src/constants/ApiUrl.js
+++ src/constants/ApiUrl.js
@@ -1,4 +1,4 @@
 const API_KEY = 'd6fc4cfcb7c484361fb2e07f31173f0e';
 
 export const ROOT_URL =
-  `http://api.openweathermap.org/data/2.5/forecast?units=metric&appid=${API_KEY}`;
+  `https://api.openweathermap.org/data/2.5/forecast?units=metric&appid=${API_KEY}`;
